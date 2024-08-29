f8a3d691af4e72a6cb7f33ab28e55fffedcd8038
gundam1337 <omarderkaoui1337@gmail.com>
2024-08-29 12:27:18 +0100

    add the server file

--- backend/server.ts
+++ backend/server.ts
@@ -1,12 +1,52 @@
-import express from 'express';
+import { Pool } from 'pg';
+//import config from './config/database';
+//import app from './app';
+//import logger from './utils/logger';
+import http from 'http';
+import dotenv from 'dotenv';
 
-const app = express();
-const port = 5000;
+dotenv.config({ path: './config.env' });
+const accessTokenSecret = process.env.ACCESS_TOKEN_SECRET;
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
+// const pool = new Pool({
+//   connectionString: config.uri,
+// });
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
-});
+const startServer = async (): Promise<void> => {
+  try {
+    // Test database connection
+    //await pool.query('SELECT NOW()');
+    //logger.info('Connected to PostgreSQL');
+
+    // Create HTTP server from the Express app
+    const httpServer = http.createServer(app);
+
+    // Listen on the created HTTP server
+    httpServer.listen(config.port, () => {
+      logger.info(`Server is listening at port ${config.port}`);
+    });
+
+    // Graceful shutdown logic
+    const handleExit = async (signal: string): Promise<void> => {
+      logger.info(
+        `Received ${signal}. Closing server and database connection...`
+      );
+      httpServer.close(() => {
+        logger.info('Server closed');
+        pool.end().then(() => {
+          logger.info('Database connection closed');
+          process.exit(0);
+        });
+      });
+    };
+
+    process.on('SIGINT', () => handleExit('SIGINT'));
+    process.on('SIGTERM', () => handleExit('SIGTERM'));
+    process.on('SIGQUIT', () => handleExit('SIGQUIT'));
+  } catch (err) {
+    logger.error('Error connecting to the database', err);
+    process.exit(1);
+  }
+};
+
+startServer();
