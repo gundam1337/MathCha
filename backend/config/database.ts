234815b3ecd4456b3ab5a67ac9fa8cadf4ed5ed8
gundam1337 <omarderkaoui1337@gmail.com>
2024-08-30 12:25:56 +0100

    connect to the database

--- backend/config/database.ts
+++ backend/config/database.ts
@@ -1,12 +1,9 @@
 import * as dotenv from 'dotenv';
-import { join } from 'path';
 
-dotenv.config({ path: join(__dirname, "../config.env") });
+dotenv.config();
 
 interface Config {
     port: number;
-    // Uncomment the next line if you're using JWT authentication
-    // jwtSecret: string;
     DB_HOST: string;
     DB_PORT: number;
     DB_USERNAME: string;
@@ -17,8 +14,6 @@ interface Config {
 
 const config: Config = {
     port: parseInt(process.env.PORT || '3001', 10),
-    // Uncomment the next line if you're using JWT authentication
-    // jwtSecret: process.env.JWT_SECRET || 'A very long secret key',
     DB_HOST: process.env.DB_HOST || 'localhost',
     DB_PORT: parseInt(process.env.DB_PORT || '5432', 10),
     DB_USERNAME: process.env.DB_USERNAME || '',
@@ -29,7 +24,5 @@ const config: Config = {
 
 // Create the connection string for PostgreSQL
 config.connectionString = `postgresql://${config.DB_USERNAME}:${config.DB_PASSWORD}@${config.DB_HOST}:${config.DB_PORT}/${config.DB_DATABASE}`;
-
-// console.log(config);
 
 export default config;
--- backend/server.ts
+++ backend/server.ts
@@ -5,7 +5,7 @@ import logger from './utils/logger';
 import http from 'http';
 import dotenv from 'dotenv';
 
-dotenv.config({ path: './config.env' });
+dotenv.config();
 const accessTokenSecret = process.env.ACCESS_TOKEN_SECRET;
 
 const pool = new Pool({
